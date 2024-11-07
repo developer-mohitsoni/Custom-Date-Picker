9e7de5f52dd8bc5028b0dfa92eb56f036a6f79bf
Mohit Soni <developer_mohitsoni@outlook.com>
2024-11-07 15:30:45 +0530

    Add gradient background and text color to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import { DatePicker } from "@/components/ui/date-picker";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Hello World</h1>
+    <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-br from-purple-800 via-indigo-600 to-purple-800 text-white">
       <DatePicker />
     </div>
   );
